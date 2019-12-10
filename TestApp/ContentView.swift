7baad0f3ffaed3bb5116bfe901f93b8eb257801a
sashitani ayumi <sasitani_611@yahoo.co.jp>
2019-12-10 21:13:25 +0900

    feat: test commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Apple on 2019/12/10.
+//  Copyright © 2019 Ayumi Sashitani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    } 
+}
